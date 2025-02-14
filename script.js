9160795946131449b2d4d686cb3f1c9c35da8ef0
Niavaranahmadi <niavaran834@gmail.com>
2025-02-14 15:54:03 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,193 @@
+// console.log("hello world");
+
+// console.log('hello good morning');
+// console.log('salam khubi che khabar');
+
+
+// operators 
+
+
+// console.log(4+5);
+// console.log("4+5");
+// console.log(45-50);
+// console.log(6*5);
+// console.log(78/2);
+
+
+// variable 
+
+// let a = 15;
+// console.log("a");
+// console.log(a);
+
+
+// var a = 12;
+// console.log(a);
+
+
+// const a = 50;
+// console.log(a);
+
+
+
+// let a = 45;
+// a = a+5;
+// console.log(a);
+
+
+// let a = 40;
+// a = 20;
+// console.log(a);
+
+
+// var ali = 45;
+// ali = ali-2;
+// console.log(ali);
+
+
+// const parsa = 10;
+// parsa = parsa+2;
+// console.log(parsa);
+
+
+
+// const a = "hello";
+// console.log(a);
+// console.log(typeof a);
+
+
+
+// const b = 142;
+// console.log(typeof b);
+
+
+
+// const colors = ["blue" , "green" , "red"];
+// console.log(colors);
+
+// const info = {
+//     name : "parsa",
+//     family : "ahmad zade"
+// };
+// console.log(info);
+// console.log(info["name"])
+
+
+
+// let names = ["parsa" , "reza" , "mohammad"];
+// console.log(names);
+
+
+
+
+// const a = 4<5;
+// console.log(a);
+
+// const a = 4>5;
+// console.log(a);
+
+
+
+// let a = 4;
+// let b = "4";
+// console.log(a==b);
+// console.log(a===b);
+
+
+
+// if (4>5){
+//     console.log("yes");
+// }else{
+//     console.log("no");
+// };
+
+
+// if (4<5){
+//     console.log("yes");
+// }else{
+//     console.log("no");
+// };
+
+
+
+// let a = 45;
+// let b = 20;
+// if (a+b==60){
+//     console.log("yes");
+// }else{
+//     console.log("no");
+// };
+
+
+
+
+// let a = 70;
+// let b = 30;
+// let c = 40;
+// if (a>100){
+//     console.log("hello");
+// }else if (b+c==a){
+//     console.log("hey");
+// }else{
+//     console.log("bye");
+// };
+
+
+
+
+// let form = document.getElementById("form");
+// form.addEventListener("submit" , function(){
+//     let name = document.getElementById("name").value;
+//     let age = document.getElementById("age").value;
+//     if (age>=18){
+//         alert("welcome " + name);
+//     }else{
+//         alert("error")
+//     }
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
